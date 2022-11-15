819af14e1f342985f1baebeeab146d05b429c737
kremnev8 <kremnev8super@yandex.ru>
2022-11-15 21:28:51 +0300

    Improve give command. Allow to give food

--- ChatCommands/ChatCommands/Chat/Commands/GiveCommandHandler.cs
+++ ChatCommands/ChatCommands/Chat/Commands/GiveCommandHandler.cs
@@ -19,7 +19,17 @@ public class GiveCommandHandler : IChatCommandHandler
         {
             return new CommandOutput("Please enter item name", Color.red);
         }
-        
+
+        if (parameters.Length > 3 && parameters[0] == "food")
+        {
+            return GiveFood(parameters);
+        }
+
+        return NormalGive(parameters);
+    }
+
+    private CommandOutput GiveFood(string[] parameters)
+    {
         int count = 1;
         int nameArgCount = parameters.Length;
         if (nameArgCount > 1 && int.TryParse(parameters[^1], out int val))
@@ -28,18 +38,82 @@ public class GiveCommandHandler : IChatCommandHandler
             nameArgCount--;
         }
 
+        string args = parameters.Take(nameArgCount).TakeLast(nameArgCount - 1).Join(null, " ");
+        if (!args.Contains('+'))
+        {
+            return new CommandOutput("Plus sign '+' is missing. Please use it to separate item names!", Color.red);
+        }
+
+        string[] itemName = args.Split(" + ");
+        if (itemName.Length > 2)
+        {
+            return new CommandOutput("Too many items in input. Please provide only two items", Color.red);
+        }
+
+        CommandOutput output1 = ParseItemName(itemName[0], out ObjectID item1);
+        if (item1 == ObjectID.None)
+            return output1;
+        CommandOutput output2 = ParseItemName(itemName[1], out ObjectID item2);
+        if (item2 == ObjectID.None)
+            return output2;
+
+        if (!PugDatabase.HasComponent<CookingIngredientCD>(item1))
+        {
+            return new CommandOutput($"{item1} is not a cooking ingredient!", Color.red);
+        }
+
+        if (!PugDatabase.HasComponent<CookingIngredientCD>(item2))
+        {
+            return new CommandOutput($"{item2} is not a cooking ingredient!", Color.red);
+        }
+        
+        CookingIngredientCD ingredient1 = PugDatabase.GetComponent<CookingIngredientCD>(item1);
+
+        AddToInventory(ingredient1.turnsIntoFood, count, int.Parse($"{(int)item1}{(int)item2}"));
+        return $"Successfully added {count} {ingredient1.turnsIntoFood}";
+    }
+
+    private CommandOutput NormalGive(string[] parameters)
+    {
+        int count = 1;
+        int variation = 0;
+        int nameArgCount = parameters.Length;
+        if (nameArgCount > 1 && int.TryParse(parameters[^1], out int val))
+        {
+            count = val;
+            nameArgCount--;
+        }
+
+        if (nameArgCount > 1 && int.TryParse(parameters[^2], out val))
+        {
+            variation = count;
+            count = val;
+            nameArgCount--;
+        }
+
         string fullName = parameters.Take(nameArgCount).Join(null, " ");
         fullName = fullName.ToLower();
 
+        CommandOutput output = ParseItemName(fullName, out ObjectID objectID);
+        if (objectID == ObjectID.None)
+            return output;
+
+        AddToInventory(objectID, count, variation);
+        return $"Successfully added {count} {objectID}, variation {variation}";
+    }
+
+    public static CommandOutput ParseItemName(string fullName, out ObjectID objectID)
+    {
         if (Enum.TryParse(fullName, out ObjectID objId))
         {
-            AddToInventory(objId, count);
-            return $"Successfully added {count} {parameters[0]}";
+            objectID = objId;
+            return "";
         }
 
         string[] keys = friendlyNameDict.Keys.Where(s => s.Contains(fullName)).ToArray();
         if (keys.Length == 0)
         {
+            objectID = ObjectID.None;
             return new CommandOutput($"No item named '{fullName}' found!", Color.red);
         }
 
@@ -48,54 +122,39 @@ public class GiveCommandHandler : IChatCommandHandler
             try
             {
                 string key = keys.First(s => s.Equals(fullName));
-                AddToInventory(friendlyNameDict[key], count);
-                return $"Successfully added {count} {keys[0]}";
+                objectID = friendlyNameDict[key];
+                return "";
             }
             catch (Exception)
             {
-                return new CommandOutput($"Ambigous match ({keys.Length} results):\n{keys.Take(10).Join(null, "\n")}{(keys.Length > 10 ? "\n..." : "")}", Color.red);
+                objectID = ObjectID.None;
+                return new CommandOutput(
+                    $"Ambigous match ({keys.Length} results):\n{keys.Take(10).Join(null, "\n")}{(keys.Length > 10 ? "\n..." : "")}",
+                    Color.red);
             }
         }
 
-        AddToInventory(friendlyNameDict[keys[0]], count);
-        return $"Successfully added {count} {keys[0]}";
+        objectID = friendlyNameDict[keys[0]];
+        return "";
     }
-
-    public string GetDescription()
-    {
-        return
-            "Use /give to give yourself any item. \n/give {itemName} [count]\nThe count parameter defaults to 1.";
-    }
-
-    public string[] GetTriggerNames()
-    {
-        return new[] { "give" };
-    }
-
-    private bool GetCount(string[] parameters, out int count)
-    {
-        count = 1;
-        if (parameters.Length == 2)
-        {
-            if (int.TryParse(parameters[1], out int val))
-            {
-                count = val;
-            }
-            else
-            {
-                return false;
-            }
-        }
-
-        return true;
-    }
-
-    private void AddToInventory(ObjectID objId, int amount)
+    
+    private void AddToInventory(ObjectID objId, int amount, int variation)
     {
         PlayerController player = GameManagers.GetMainManager().player;
         if (player == null) return;
         InventoryHandler handler = player.playerInventoryHandler;
         if (handler == null) return;
-        handler.CreateItem(0, objId, amount, player.WorldPosition, 0);
+        handler.CreateItem(0, objId, amount, player.WorldPosition, variation);
+    }
+
+    public string GetDescription()
+    {
+        return
+            "Use /give to give yourself any item. \n/give {itemName} [count] [variation]\nThe count parameter defaults to 1. Variation defaults to 0\n/give food {item1} + {item2} [count] Add any food. First item is used as a base ingredient.";
+    }
+
+    public string[] GetTriggerNames()
+    {
+        return new[] { "give" };
     }
 }
