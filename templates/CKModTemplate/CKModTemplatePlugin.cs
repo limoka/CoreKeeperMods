3878feb0e0c3cca411869528d1bfa54ed659f10c
kremnev8 <kremnev8super@yandex.ru>
2022-11-29 16:16:42 +0300

    Update template

--- templates/CKModTemplate/CKModTemplatePlugin.cs
+++ templates/CKModTemplate/CKModTemplatePlugin.cs
@@ -1,23 +1,23 @@
 ﻿using BepInEx;
-using BepInEx.IL2CPP;
+using BepInEx.Unity.IL2CPP;
 using BepInEx.Logging;
 using HarmonyLib;
 
 namespace CKModTemplate
 {
     [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
-    [BepInDependency("com.le4fless.corelib")]
-    [BepInProcess("CoreKeeper.exe")]
+    [BepInDependency(CoreLibPlugin.GUID)]
     public class CKModTemplatePlugin : BasePlugin
     {
         public static ManualLogSource logger;
+		
         public override void Load()
         {
             // Plugin startup logic
             logger = Log;
-            logger.LogInfo($"{PluginInfo.PLUGIN_NAME} mod is loaded!");
             Harmony harmony = new Harmony(PluginInfo.PLUGIN_GUID);
             harmony.PatchAll();
+			logger.LogInfo($"{PluginInfo.PLUGIN_NAME} mod is loaded!");
         }
     }
 }
