32162bec4e5dddec3cf3e5ac46f00462f349a8e4
kremnev8 <kremnev8super@yandex.ru>
2023-03-27 10:50:35 +0300

    Fix Infinite Ore Boulder mod

--- InfiniteOreBoulder/InfiniteOreBoulder/InfiniteOreBoulderSystem.cs
+++ InfiniteOreBoulder/InfiniteOreBoulder/InfiniteOreBoulderSystem.cs
@@ -1,8 +1,12 @@
 ﻿using System;
+using CoreLib.Submodules.ModComponent;
 using CoreLib.Submodules.ModSystem;
+using CoreLib.Util.Extensions;
 using PugAutomation;
 using Unity.Collections;
 using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Transforms;
 using UnityEngine;
 
 namespace InfiniteOreBoulder
@@ -10,15 +14,32 @@ namespace InfiniteOreBoulder
     public class InfiniteOreBoulderSystem : MonoBehaviour, IPseudoServerSystem
     {
         internal World serverWorld;
-        
+        internal EntityQuery query;
+        internal ModComponentDataFromEntity<HealthCD> healthGroup;
+
         private float waitTime;
-        private const float refreshTime = 5;
-        
+        private const float refreshTime = 30;
+
         public InfiniteOreBoulderSystem(IntPtr ptr) : base(ptr) { }
 
         public void OnServerStarted(World world)
         {
             serverWorld = world;
+            query = serverWorld.EntityManager.CreateEntityQuery(
+                new EntityQueryDesc
+                {
+                    All = new[]
+                    {
+                        ComponentType.ReadOnly<DropsLootWhenDamagedCD>(),
+                        ComponentType.ReadOnly<PugAutomationCD>(),
+                        ComponentType.ReadOnly<MineableDamageDecreaseCD>(),
+                        ComponentType.ReadOnly<HealthCD>()
+                    },
+                    Any = Array.Empty<ComponentType>(),
+                    None = new[] { ComponentType.ReadOnly<EntityDestroyedCD>() },
+                    Options = EntityQueryOptions.IncludeDisabled
+                });
+            healthGroup = new ModComponentDataFromEntity<HealthCD>(serverWorld.EntityManager);
         }
 
         public void OnServerStopped()
@@ -26,34 +47,27 @@ namespace InfiniteOreBoulder
             serverWorld = null;
         }
 
-        private void Update()
+        private void FixedUpdate()
         {
             if (serverWorld == null) return;
-            
-            waitTime -= Time.deltaTime;
+
+            waitTime -= Time.fixedDeltaTime;
 
             if (waitTime <= 0)
             {
-                EntityQuery query =
-                    serverWorld.EntityManager.CreateEntityQuery(
-                        ComponentType.ReadOnly<DropsLootWhenDamagedCD>(),
-                        ComponentType.ReadOnly<PugAutomationCD>(),
-                        ComponentType.ReadOnly<MineableDamageDecreaseCD>(),
-                        ComponentType.ReadOnly<HealthCD>());
-
+                healthGroup = new ModComponentDataFromEntity<HealthCD>(serverWorld.EntityManager);
                 NativeArray<Entity> result = query.ToEntityArray(Allocator.Temp);
 
                 foreach (Entity entity in result)
                 {
-                    HealthCD healthCd = serverWorld.EntityManager.GetComponentData<HealthCD>(entity);
-                    
+                    HealthCD healthCd = healthGroup[entity];
                     if (healthCd.health < healthCd.maxHealth - 1000)
                     {
                         healthCd.health = healthCd.maxHealth;
-                        serverWorld.EntityManager.SetComponentData(entity, healthCd);
+                        healthGroup[entity] = healthCd;
                     }
                 }
-                
+
                 waitTime = refreshTime;
             }
         }
