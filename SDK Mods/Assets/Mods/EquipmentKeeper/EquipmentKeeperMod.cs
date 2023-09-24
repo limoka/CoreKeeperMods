c5554787dd4dbbff6772be31f29515bc1a977f0d
kremnev8 <kremnev8super@yandex.ru>
2023-09-24 12:43:03 +0300

    Port Equipment Keeper mod

---
+++ SDK Mods/Assets/Mods/EquipmentKeeper/EquipmentKeeperMod.cs
@@ -0,0 +1,48 @@
+﻿using System.Linq;
+using PugMod;
+using Unity.Burst;
+using UnityEngine;
+
+namespace EquipmentKeeper
+{
+    public class EquipmentKeeperMod : IMod
+    {
+        public const string VERSION = "1.1.0";
+        public const string NAME = "Equipment Keeper";
+        private LoadedMod modInfo;
+
+        public void EarlyInit()
+        {
+            Debug.Log($"[{NAME}]: Mod version: {VERSION}");
+            modInfo = GetModInfo(this);
+            if (modInfo == null)
+            {
+                Debug.Log($"[{NAME}]: Failed to load {NAME}: mod metadata not found!");
+                return;
+            }
+
+            Debug.Log($"[{NAME}]: Mod loaded successfully");
+        }
+
+        public static LoadedMod GetModInfo(IMod mod)
+        {
+            return API.ModLoader.LoadedMods.FirstOrDefault(modInfo => modInfo.Handlers.Contains(mod));
+        }
+
+        public void Init()
+        {
+        }
+
+        public void Shutdown()
+        {
+        }
+
+        public void ModObjectLoaded(Object obj)
+        {
+        }
+
+        public void Update()
+        {
+        }
+    }
+}
---
+++ SDK Mods/Assets/Mods/EquipmentKeeper/PlayerController_Patch.cs
@@ -0,0 +1,19 @@
+﻿using HarmonyLib;
+
+namespace EquipmentKeeper
+{
+    [HarmonyPatch]
+    public class PlayerController_Patch
+    {
+        [HarmonyPatch(typeof(PlayerController),"ReduceDurabilityOfAllEquipment")]
+        [HarmonyPatch(typeof(PlayerController),"ReduceDurabilityOfEquipment")]
+        [HarmonyPatch(typeof(PlayerController),"ReduceDurabilityOfHeldEquipment")]
+        [HarmonyPatch(typeof(PlayerController),"ReducePercentageDurabilityOfAllEquipment")]
+        [HarmonyPatch(typeof(PlayerController),"ReducePercentageDurabilityOfEquipment")]
+        [HarmonyPrefix]
+        static void DontReduceDurability(ref bool __runOriginal)
+        {
+            __runOriginal = false;
+        }
+    }
+}
