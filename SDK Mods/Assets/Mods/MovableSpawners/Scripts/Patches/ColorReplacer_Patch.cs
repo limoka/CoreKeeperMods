e70aea558e16e441e6047e67cab68c0ad3a92dd7
kremnev8 <kremnev8super@yandex.ru>
2023-10-05 10:27:29 +0300

    Update MovableSpawners mod code

--- SDK Mods/Assets/Mods/MovableSpawners/Scripts/Patches/ColorReplacer_Patch.cs
+++ SDK Mods/Assets/Mods/MovableSpawners/Scripts/Patches/ColorReplacer_Patch.cs
@@ -11,15 +11,15 @@ namespace MovableSpawners.Patches
 
         static ColorReplacer_Patch()
         {
-            ColorUtility.TryParseHtmlString("#ffffff", out oldColor);
+            UnityEngine.ColorUtility.TryParseHtmlString("#ffffff", out oldColor);
             
-            ColorUtility.TryParseHtmlString("#da6210", out spawnerColors[0]);
-            ColorUtility.TryParseHtmlString("#c6723b", out spawnerColors[1]);
-            ColorUtility.TryParseHtmlString("#ae5f3f", out spawnerColors[2]);
-            ColorUtility.TryParseHtmlString("#954d94", out spawnerColors[3]);
-            ColorUtility.TryParseHtmlString("#4f4b48", out spawnerColors[4]);
-            ColorUtility.TryParseHtmlString("#165dd9", out spawnerColors[5]);
-            ColorUtility.TryParseHtmlString("#7c391f", out spawnerColors[6]);
+            UnityEngine.ColorUtility.TryParseHtmlString("#da6210", out spawnerColors[0]);
+            UnityEngine.ColorUtility.TryParseHtmlString("#c6723b", out spawnerColors[1]);
+            UnityEngine.ColorUtility.TryParseHtmlString("#ae5f3f", out spawnerColors[2]);
+            UnityEngine.ColorUtility.TryParseHtmlString("#954d94", out spawnerColors[3]);
+            UnityEngine.ColorUtility.TryParseHtmlString("#4f4b48", out spawnerColors[4]);
+            UnityEngine.ColorUtility.TryParseHtmlString("#165dd9", out spawnerColors[5]);
+            UnityEngine.ColorUtility.TryParseHtmlString("#7c391f", out spawnerColors[6]);
         }
         
         [HarmonyPatch(typeof(ColorReplacer), nameof(ColorReplacer.UpdateColorReplacerFromObjectData))]
