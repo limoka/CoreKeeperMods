9dd8c3d68b61fa98436b56d52466000d017ef693
limoka8 <liza.kremneva.8@yandex.ru>
2026-03-07 21:57:16 +0300

    Patch conversion to add names where possible

---
+++ SDK Mods/Assets/Mods/UnityExplorer/Scripts/Loader/CoreKeeper/Patch/ConversionManager_Patch.cs
@@ -0,0 +1,24 @@
+﻿using HarmonyLib;
+using Pug.Conversion;
+using Unity.Collections;
+using Unity.Entities;
+using UnityEngine;
+
+namespace Mods.UnityExplorer.Scripts.Loader.CoreKeeper.Patch
+{
+    [HarmonyPatch]
+    public static class ConversionManager_Patch
+    {
+
+        [HarmonyPatch(typeof(ConversionManager), "RunConverters")]
+        [HarmonyPostfix]
+        public static void OnRunConverters(ConversionManager __instance, GameObject gameObject, Entity entity)
+        {
+            var entityManager = __instance.EntityManager;
+            var name = new FixedString64Bytes();
+            name.CopyFromTruncated(gameObject.name);
+            
+            entityManager.SetName(entity, name);
+        }
+    }
+}
