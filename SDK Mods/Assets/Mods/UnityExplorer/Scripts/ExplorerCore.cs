fdc3e96f6ca3d9765bc95df63eee97450ca19524
limoka8 <liza.kremneva.8@yandex.ru>
2026-05-09 18:49:06 +0300

    Unity Explorer: bump version - 4.13.5

--- SDK Mods/Assets/Mods/UnityExplorer/Scripts/ExplorerCore.cs
+++ SDK Mods/Assets/Mods/UnityExplorer/Scripts/ExplorerCore.cs
@@ -19,7 +19,7 @@ namespace UnityExplorer
     public static class ExplorerCore
     {
         public const string NAME = "UnityExplorer";
-        public const string VERSION = "4.13.4";
+        public const string VERSION = "4.13.5";
         public const string AUTHOR = "Sinai, yukieiji, Limoka";
         public const string GUID = "com.sinai.unityexplorer";
 
