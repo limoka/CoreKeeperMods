1399dbe63dd4d8dcd08046c8aeb6b3354771313c
limoka8 <liza.kremneva.8@yandex.ru>
2026-05-10 12:06:45 +0300

    Placement Plus: bump version - 2.1.7

--- SDK Mods/Assets/Mods/PlacementPlus/Scripts/PlacementPlusMod.cs
+++ SDK Mods/Assets/Mods/PlacementPlus/Scripts/PlacementPlusMod.cs
@@ -20,7 +20,7 @@ namespace PlacementPlus
     public class PlacementPlusMod : IMod
     {
         public const string MODNAME = "Placement Plus";
-        public const string VERSION = "2.1.6";
+        public const string VERSION = "2.1.7";
 
         public static Logger Log = new Logger(MODNAME);
         public static ConfigFile Config;
